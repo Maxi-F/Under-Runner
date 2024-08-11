a93430bb617bab84d7aff965252cfe0da26efd1f
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-08-11 15:08:34 -0300

    Add Throw ball only on stop logic

--- Assets/_Dev/GolfTest/Scripts/GolfBall/ThrowGolfBall.cs
+++ Assets/_Dev/GolfTest/Scripts/GolfBall/ThrowGolfBall.cs
@@ -8,7 +8,13 @@ namespace _Dev.GolfTest.Scripts.GolfBall
     {
         [SerializeField] private InputHandlerSO inputHandlerSo;
 
+        [Header("Throw Properties")]
         [SerializeField] private float forceMagnitude = 10.0f;
+
+        [Header("Ball properties")] 
+        [SerializeField] private float minStopVelocity = 0.01f;
+
+        [SerializeField] private float minCheckVelocity = 0.01f;
         
         private Vector3 _desiredDirection;
 
@@ -17,7 +23,9 @@ namespace _Dev.GolfTest.Scripts.GolfBall
         
         void Start()
         {
+            // TODO put this logic somewhere else, should not be here
             Cursor.lockState = CursorLockMode.Locked;
+            
             _rigidbody ??= GetComponent<Rigidbody>();
         }
         
@@ -34,13 +42,25 @@ namespace _Dev.GolfTest.Scripts.GolfBall
             inputHandlerSo.onThrow.RemoveListener(HandleThrow);
         }
 
+        private void Update()
+        {
+            if (_rigidbody.velocity.magnitude >= minCheckVelocity && 
+                _rigidbody.velocity.magnitude <= minStopVelocity)
+            {
+                _rigidbody.velocity = new Vector3(0, _rigidbody.velocity.y, 0);
+            }
+        }
+        
         private void FixedUpdate()
         {
-            if (_shouldThrow)
+            if (!_shouldThrow || _rigidbody.velocity.magnitude >= float.Epsilon)
             {
-                _rigidbody.AddForce(_desiredDirection * forceMagnitude, ForceMode.Impulse);
                 _shouldThrow = false;
+                return;
             }
+            
+            _rigidbody.AddForce(_desiredDirection * forceMagnitude, ForceMode.Impulse);
+            _shouldThrow = false;
         }
 
         void HandleThrow()
@@ -50,12 +70,17 @@ namespace _Dev.GolfTest.Scripts.GolfBall
 
         void HandleCameraRotate(Vector2 _lookDelta, bool _isController)
         {
+            if (UnityEngine.Camera.main == null)
+            {
+                Debug.LogError("Camera main is null!");
+                return;
+            }
             _desiredDirection = UnityEngine.Camera.main.transform.forward;
         }
 
         private void OnDrawGizmos()
         {
-            Gizmos.DrawLine(transform.position, _desiredDirection * 10.0f);
+            Gizmos.DrawLine(transform.position, _desiredDirection);
         }
     }
 }
