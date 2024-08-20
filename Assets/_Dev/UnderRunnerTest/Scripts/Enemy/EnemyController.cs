e18d61235089e2677b1215a702f2ddd35f5e4a8b
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-08-19 22:32:32 -0300

    Add enemy parried float down and up

--- Assets/_Dev/UnderRunnerTest/Scripts/Enemy/EnemyController.cs
+++ Assets/_Dev/UnderRunnerTest/Scripts/Enemy/EnemyController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using _Dev.GolfTest.Scripts.Events;
 using _Dev.UnderRunnerTest.Scripts.Health;
 using UnityEngine;
@@ -11,12 +12,22 @@ namespace _Dev.UnderRunnerTest.Scripts.Enemy
         [SerializeField] private GameObject enemyObject;
         [SerializeField] private GameObject shieldObject;
         [SerializeField] private bool shieldActive;
+
+        [Header("ShieldProperties")] [SerializeField]
+        private float timeToReactivateShield = 4.0f;
+        
+        [Header("FlyProperties")] 
+        [SerializeField] private float flyVelocity = 10.0f;
+        [SerializeField] private float flyTime = 2.0f;
         
         [Header("Events")]
         [SerializeField] private VoidEventChannelSO onEnemyDeathEvent;
+        [SerializeField] private BoolEventChannelSO onEnemyParriedEvent;
         [SerializeField] private IntEventChannelSO onEnemyDamageEvent;
         
         private HealthPoints _healthPoints;
+        private bool _shouldFly;
+        
         private void Start()
         {
             _healthPoints ??= GetComponent<HealthPoints>();
@@ -26,12 +37,45 @@ namespace _Dev.UnderRunnerTest.Scripts.Enemy
             onEnemyDeathEvent?.onEvent.AddListener(HandleDeath);
         }
 
+        private void Update()
+        {
+            if(_shouldFly)
+                enemyObject.transform.position +=
+                    new Vector3(
+                        0f, 
+                        (shieldObject.activeInHierarchy ? 1f : -1f) * flyVelocity * Time.deltaTime, 
+                        0f
+                        );
+        }
+
         public void HandleShield(bool isActive)
         {
             Debug.Log($"Handling shield: {isActive}");
             
             shieldObject.SetActive(isActive);
             _healthPoints.SetCanTakeDamage(!isActive);
+            onEnemyParriedEvent?.RaiseEvent(isActive);
+            
+            StartCoroutine(Fly(isActive));
+        }
+
+        private IEnumerator Fly(bool isActive)
+        {
+            _shouldFly = true;
+
+            yield return new WaitForSeconds(flyTime);
+
+            _shouldFly = false;
+
+            if(!isActive)
+                StartCoroutine(ReactivateShield());
+        }
+
+        private IEnumerator ReactivateShield()
+        {
+            yield return new WaitForSeconds(timeToReactivateShield);
+            
+            HandleShield(true);
         }
 
         private void OnDisable()
