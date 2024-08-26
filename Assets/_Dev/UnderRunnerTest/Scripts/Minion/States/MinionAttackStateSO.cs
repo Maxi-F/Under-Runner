429cb42427860298e19c77d272b0d1aa26d5f0fb
Nicodragon25 <nicolasariellaure@gmail.com>
2024-08-26 04:08:15 -0300

    Add minion attack state

---
+++ Assets/_Dev/UnderRunnerTest/Scripts/Minion/States/MinionAttackStateSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace _Dev.UnderRunnerTest.Scripts.Minion.States
+{
+    [CreateAssetMenu(fileName = "MinionAttackState", menuName = "Minion/AttackState", order = 0)]
+    public class MinionAttackStateSO : MinionStateSO
+    {
+    
+    }
+}
