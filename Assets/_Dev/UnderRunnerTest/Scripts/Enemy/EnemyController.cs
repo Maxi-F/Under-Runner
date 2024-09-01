5d66973e5828087e8456400e5a155603bc95eb92
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-09-01 19:48:34 -0300

    Make enemy not float on parried

--- Assets/_Dev/UnderRunnerTest/Scripts/Enemy/EnemyController.cs
+++ Assets/_Dev/UnderRunnerTest/Scripts/Enemy/EnemyController.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
+using _Dev.UnderRunnerTest.Scripts.Enemy.Shield;
 using _Dev.UnderRunnerTest.Scripts.Events;
 using _Dev.UnderRunnerTest.Scripts.Health;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace _Dev.UnderRunnerTest.Scripts.Enemy
 {
@@ -10,16 +12,15 @@ namespace _Dev.UnderRunnerTest.Scripts.Enemy
     {
         [Header("Properties")] 
         [SerializeField] private GameObject enemyObject;
-        [SerializeField] private GameObject shieldObject;
+        [SerializeField] private ShieldController shieldController;
         [SerializeField] private bool shieldActive;
 
-        [Header("ShieldProperties")] [SerializeField]
-        private float timeToReactivateShield = 4.0f;
-        [SerializeField] private HealthPoints shieldPoints;
+        [Header("ShieldProperties")] 
+        [SerializeField] private float timeToReactivateShield = 4.0f;
+
+        [SerializeField] private float timeToStartReactivatingShield = 2.0f;
         
-        [Header("FlyProperties")] 
-        [SerializeField] private float flyVelocity = 10.0f;
-        [SerializeField] private float flyTime = 2.0f;
+        [SerializeField] private HealthPoints shieldPoints;
         
         [Header("Events")]
         [SerializeField] private VoidEventChannelSO onEnemyDeathEvent;
@@ -27,7 +28,6 @@ namespace _Dev.UnderRunnerTest.Scripts.Enemy
         [SerializeField] private IntEventChannelSO onEnemyDamageEvent;
         
         private HealthPoints _healthPoints;
-        private bool _shouldFly;
         
         private void Start()
         {
@@ -38,57 +38,34 @@ namespace _Dev.UnderRunnerTest.Scripts.Enemy
             onEnemyDeathEvent?.onEvent.AddListener(HandleDeath);
         }
 
-        private void Update()
-        {
-            if(_shouldFly)
-                enemyObject.transform.position +=
-                    new Vector3(
-                        0f, 
-                        (shieldObject.activeInHierarchy ? 1f : -1f) * flyVelocity * Time.deltaTime, 
-                        0f
-                        );
-        }
-
         public void HandleShield(bool isActive)
         {
-            if (!isActive && !shieldObject.activeInHierarchy) return;
+            if (!isActive && !shieldController.IsActive()) return;
             
-            shieldObject.SetActive(isActive);
+            shieldController.SetActive(isActive);
             _healthPoints.SetCanTakeDamage(!isActive);
 
-            if (!isActive)
-            {
-                onEnemyParriedEvent?.RaiseEvent(isActive);
-            }
-            else
+            onEnemyParriedEvent?.RaiseEvent(isActive);
+           
+            if (isActive)
             {
                 shieldPoints.ResetHitPoints();
             }
-            
-            StartCoroutine(Fly(isActive));
-        }
-
-        private IEnumerator Fly(bool isActive)
-        {
-            _shouldFly = true;
-
-            yield return new WaitForSeconds(flyTime);
-
-            _shouldFly = false;
-
-            if (!isActive)
-            {
-                StartCoroutine(ReactivateShield());
-            }
             else
             {
-                onEnemyParriedEvent?.RaiseEvent(isActive);
+                StartCoroutine(ReactivateShield());
             }
         }
 
         private IEnumerator ReactivateShield()
         {
+            yield return new WaitForSeconds(timeToStartReactivatingShield);
+            
+            shieldController.SetIsActivating(true);
+            
             yield return new WaitForSeconds(timeToReactivateShield);
+            
+            shieldController.SetActiveMaterial();
             
             HandleShield(true);
         }
