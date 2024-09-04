48f3a3e2602528a27130366d005464c1b362b9fc
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-09-03 21:13:27 -0300

    Add New Movement Handler for Road Manager

--- Assets/_Dev/UnderRunnerTest/Scripts/Roads/RoadManager.cs
+++ Assets/_Dev/UnderRunnerTest/Scripts/Roads/RoadManager.cs
@@ -11,20 +11,22 @@ namespace _Dev.UnderRunnerTest.Scripts.Roads
         [SerializeField] private GameObject startingLastRoad;
         [SerializeField] private int initRoadCount = 7;
         [SerializeField] private int maxRoads = 7;
+        [SerializeField] private Vector3 roadsInitVelocity = new Vector3(0f, 0f, -20f);
         
         [Header("Events")]
         [SerializeField] private VoidEventChannelSO onNewRoadTriggerEvent;
-
         [SerializeField] private VoidEventChannelSO onRoadDeleteTriggerEvent;
-        
         [SerializeField] private GameObjectEventChannelSO onRoadInstantiatedEvent;
-
+        [SerializeField] private Vector3EventChannelSO onNewVelocityEvent;
+        
         private int _roadCount;
         private int _actualIndex = 0;
+        private Vector3 _roadsVelocity;
         private GameObject _lastRoad;
 
-        public void Start()
+        public void OnEnable()
         {
+            _roadsVelocity = roadsInitVelocity;
             _roadCount = initRoadCount;
             _lastRoad = startingLastRoad;
             onNewRoadTriggerEvent?.onEvent.AddListener(HandleNewRoad);
@@ -35,6 +37,12 @@ namespace _Dev.UnderRunnerTest.Scripts.Roads
         {
             onNewRoadTriggerEvent?.onEvent.RemoveListener(HandleNewRoad);
             onRoadDeleteTriggerEvent?.onEvent.RemoveListener(HandleDeleteRoad);
+        }
+
+        public void HandleNewVelocity(Vector3 velocity)
+        {
+            _roadsVelocity = velocity;
+            onNewVelocityEvent?.RaiseEvent(velocity);
         }
 
         private void HandleDeleteRoad()
@@ -51,6 +59,9 @@ namespace _Dev.UnderRunnerTest.Scripts.Roads
             GameObject newLastRoad = Instantiate(roads[_actualIndex].roadSection, roadEnd.transform.position,
                 roads[_actualIndex].startRotation);
 
+            Movement roadMovement = newLastRoad.GetComponentInChildren<Movement>();
+            roadMovement.SetVelocity(_roadsVelocity);
+            
             onRoadInstantiatedEvent?.RaiseEvent(newLastRoad);
             
             _lastRoad = newLastRoad;
