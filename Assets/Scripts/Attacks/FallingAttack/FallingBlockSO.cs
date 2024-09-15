07931934e8098544d2dc4ef479533ce96dd894de
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-09-15 15:19:13 -0300

    Fix fileName & menuName

--- Assets/Scripts/Attacks/FallingAttack/FallingBlockSO.cs
+++ Assets/Scripts/Attacks/FallingAttack/FallingBlockSO.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Attacks.FallingAttack
 {
-    [CreateAssetMenu(menuName = "Falling Block Config", fileName = "FallingBlockConfig", order = 0)]
+    [CreateAssetMenu(fileName = "FallingAttackConfig", menuName = "Enemy/Attacks/FallingAttackConfig", order = 0)]
     public class FallingBlockSO : ScriptableObject
     {
         public Vector3 initPosition;
