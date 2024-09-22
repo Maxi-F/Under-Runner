7bfe6297e35fb4d5d0ba05ad9f8625510853fca6
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-09-21 21:02:43 -0300

    Refactor Attack Controller for readability

--- Assets/Scripts/Minion/States/MinionAttackController.cs
+++ Assets/Scripts/Minion/States/MinionAttackController.cs
@@ -13,8 +13,6 @@ namespace Minion.States
         [SerializeField] private float ChargeLength;
         [SerializeField] private float ChargeSpeed;
 
-        [SerializeField] private Material defaultMat;
-        [SerializeField] private Material chargeMat;
         [SerializeField] private GameObjectEventChannelSO onCollidePlayerEventChannel;
 
         [SerializeField] private MinionAgent minionAgent;
@@ -22,12 +20,17 @@ namespace Minion.States
         private Vector3 _dir;
 
         private LineRenderer _aimLine;
+        private HealthPoints _healthPoints;
+        private Collider _collider;
 
-
+        private bool _isCharging;
+        
         public override void Enter()
         {
-            _aimLine = agentTransform.Find("AimLine").gameObject.GetComponent<LineRenderer>();
-
+            _aimLine ??= gameObject.transform.Find("AimLine").gameObject.GetComponent<LineRenderer>();
+            _healthPoints ??= GetComponent<HealthPoints>();
+            _collider ??= GetComponent<Collider>();
+            
             onCollidePlayerEventChannel.onGameObjectEvent.AddListener(DealDamage);
 
             StartCoroutine(AttackCoroutine());
@@ -42,58 +45,69 @@ namespace Minion.States
             onCollidePlayerEventChannel.onGameObjectEvent.RemoveListener(DealDamage);
         }
 
+        private void StartAimLine()
+        {
+            _aimLine.SetPosition(0, transform.position);
+            _aimLine.SetPosition(1, transform.position);
+            _aimLine.enabled = true;
+        }
+
+        private void SetNewAimPosition(float timer)
+        {
+            _dir = target.transform.position - transform.position;
+            _dir.y = 0;
+
+            Vector3 aimPosition = Vector3.Lerp(transform.position, transform.position + _dir.normalized * ChargeLength, timer / preparationDuration);
+            _aimLine.SetPosition(1, aimPosition);
+        }
+        
         private IEnumerator AttackCoroutine()
         {
             float timer = 0;
             float startTime = Time.time;
-            _aimLine.SetPosition(0, agentTransform.position);
-            _aimLine.SetPosition(1, agentTransform.position);
-            _aimLine.enabled = true;
+            StartAimLine();
 
             Debug.Log("Start preparation");
             while (timer < preparationDuration)
             {
                 timer = Time.time - startTime;
-                _dir = target.transform.position - agentTransform.position;
-                _dir.y = 0;
-
-                Vector3 aimPosition = Vector3.Lerp(agentTransform.position, agentTransform.position + _dir.normalized * ChargeLength, timer / preparationDuration);
-                _aimLine.SetPosition(1, aimPosition);
+                SetNewAimPosition(timer);
                 yield return null;
             }
 
             _aimLine.enabled = false;
             Debug.Log("Start Charge");
 
-            Vector3 destination = agentTransform.position + _dir.normalized * ChargeLength;
-            Vector3 startPosition = agentTransform.position;
-            timer = 0;
-            float chargeDuration = ChargeLength / ChargeSpeed;
-            startTime = Time.time;
-            agentTransform.GetComponent<HealthPoints>().SetCanTakeDamage(false);
-            foreach (MeshRenderer meshRenderer in agentTransform.GetComponentsInChildren<MeshRenderer>())
-            {
-                meshRenderer.material = chargeMat;
-            }
+            _isCharging = true;
+            StartCoroutine(StartCharge());
+            yield return new WaitUntil(() => !_isCharging);
 
-            agentTransform.GetComponent<Collider>().isTrigger = true;
+            _healthPoints.SetCanTakeDamage(true);
+            _collider.isTrigger = false;
+            
+            minionAgent.ChangeStateToIdle();
+        }
+
+        private IEnumerator StartCharge()
+        {
+            float timer = 0;
+            float chargeDuration = ChargeLength / ChargeSpeed;
+            float startTime = Time.time;
+            
+            Vector3 destination = transform.position + _dir.normalized * ChargeLength;
+            Vector3 startPosition = transform.position;
+            
+            _healthPoints.SetCanTakeDamage(false);
+            _collider.isTrigger = true;
+            
             while (timer < chargeDuration)
             {
                 timer = Time.time - startTime;
-                agentTransform.position = Vector3.Lerp(startPosition, destination, timer / chargeDuration);
+                transform.position = Vector3.Lerp(startPosition, destination, timer / chargeDuration);
                 yield return null;
             }
 
-            agentTransform.GetComponent<HealthPoints>().SetCanTakeDamage(true);
-
-            foreach (MeshRenderer meshRenderer in agentTransform.GetComponentsInChildren<MeshRenderer>())
-            {
-                meshRenderer.material = defaultMat;
-            }
-
-            agentTransform.GetComponent<Collider>().isTrigger = false;
-            
-            minionAgent.ChangeStateToIdle();
+            _isCharging = false;
         }
 
         private void DealDamage(GameObject target)
