b730e4cd46709dc4d1316b57b700d98b82d0013b
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-09-23 22:20:26 -0300

    Create Minion SO

---
+++ Assets/Scripts/Minion/ScriptableObjects/MinionSO.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Minion.ScriptableObjects
+{
+    [Serializable]
+    public class ChargeAttack
+    {
+        public float duration;
+        public float length;
+    }
+
+    [Serializable]
+    public class Attack
+    {
+        public int damage;
+        public float speed;
+    }
+
+    [Serializable]
+    public class Idle
+    {
+        public float minIdleTime;
+        public float maxIdleTime;
+    }
+
+    [Serializable]
+    public class Move
+    {
+        public float minMovingTime;
+        public float maxMovingTime;
+        public float speed;
+        public float minDistance;
+    }
+    
+    [CreateAssetMenu(menuName = "Minions/Config")]
+    public class MinionSO : ScriptableObject
+    {
+        public ChargeAttack chargeAttackData;
+        public Attack attackData;
+        public Idle idleData;
+        public Move moveData;
+
+        public float GetRandomIdleTime()
+        {
+            return Random.Range(idleData.minIdleTime, idleData.maxIdleTime);
+        }
+        
+        public float GetRandomMoveTime()
+        {
+            return Random.Range(moveData.minMovingTime, moveData.maxMovingTime);
+        }
+    }
+}
