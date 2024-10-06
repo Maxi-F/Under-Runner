0ef16aa2f9af065080232b79c0e4a6a09762f236
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-10-05 21:48:20 -0300

    Add Scenes Handler

---
+++ Assets/Scripts/Scenes/ScenesHandler.cs
@@ -0,0 +1,67 @@
+using System;
+using Managers;
+using UnityEngine;
+
+namespace Scenes
+{
+    public class SceneHandler : MonoBehaviour
+    {
+        [SerializeField] private string[] scenesToSubscribeTo;
+        [Tooltip("Current scene name")] [SerializeField] private string sceneName;
+        [Tooltip("Optional scenes to activate with the current scene")] [SerializeField] private string[] optionalScenes = new string[] {};
+        
+        private SceneryManager _sceneryManager;
+        
+        private void OnEnable()
+        {
+            _sceneryManager = FindObjectOfType<SceneryManager>();
+
+            foreach (var optionalScene in optionalScenes)
+            {
+                _sceneryManager.LoadScene(optionalScene);
+            }
+
+            SubscribeToActions();
+        }
+
+        private void OnDisable()
+        {
+            UnsubscribeToActions();
+        }
+
+        /// <summary>
+        /// Subscribes to add scene event of the scenes to subscribe to.
+        /// </summary>
+        private void SubscribeToActions()
+        {
+            Array.ForEach(scenesToSubscribeTo, (aSceneName) =>
+            {
+                _sceneryManager.SubscribeEventToAddScene(aSceneName, UnloadScene);
+            });
+        }
+        
+        /// <summary>
+        /// Unsubscribes to add scene event of the scenes to subscribe to.
+        /// </summary>
+        private void UnsubscribeToActions()
+        {
+            Array.ForEach(scenesToSubscribeTo, (aSceneName) =>
+            {
+                _sceneryManager.UnsubscribeEventToAddScene(aSceneName, UnloadScene);
+            });
+        }
+        
+        /// <summary>
+        /// Unloads current scene and optional scenes.
+        /// </summary>
+        private void UnloadScene()
+        {
+            _sceneryManager.UnloadScene(sceneName);
+
+            foreach (var optionalScene in optionalScenes)
+            {
+                _sceneryManager.UnloadScene(optionalScene);
+            }
+        }
+    }
+}
