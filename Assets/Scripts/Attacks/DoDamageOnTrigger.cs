93d18a21d5d610f5a160b4d420f1ca7108493e25
Nicodragon25 <nicolasariellaure@gmail.com>
2024-10-06 18:14:11 -0300

    Fix enemy swing not triggering player dodge

--- Assets/Scripts/Attacks/DoDamageOnTrigger.cs
+++ Assets/Scripts/Attacks/DoDamageOnTrigger.cs
@@ -7,20 +7,14 @@ namespace Attacks
     public class DoDamageOnTrigger : MonoBehaviour
     {
         [SerializeField] private int damage = 5;
-        [SerializeField] private bool isAvoidable = false;
         [SerializeField] private string entityDamageTaker = "Player";
-        
-        private IEnumerator OnTriggerEnter(Collider other)
+
+        private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag(entityDamageTaker))
             {
                 ITakeDamage damageTaker = other.GetComponent<ITakeDamage>();
-
-                yield return null;
-                if(isAvoidable)
-                    damageTaker.TryTakeAvoidableDamage(damage);
-                else 
-                    damageTaker.TryTakeDamage(damage);
+                damageTaker.TryTakeDamage(damage);
             }
         }
     }
