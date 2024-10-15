c71ab480590467a2f79709d50c00beb3c3e3e639
Nicodragon25 <nicolasariellaure@gmail.com>
2024-10-14 23:55:40 -0300

    Update playerAnimation strings to hash

--- Assets/Scripts/Player/Animation/PlayerAnimationHandler.cs
+++ Assets/Scripts/Player/Animation/PlayerAnimationHandler.cs
@@ -9,6 +9,11 @@ public class PlayerAnimationHandler : MonoBehaviour
     [SerializeField] private PlayerAnimationConfigSO animationConfig;
 
     private Animator _animator;
+    
+    private static readonly int VelocityZ = Animator.StringToHash("velocity_z");
+    private static readonly int VelocityX = Animator.StringToHash("velocity_x");
+    private static readonly int Attack = Animator.StringToHash("Attack");
+    private static readonly int AttackProgress = Animator.StringToHash("attackProgress");
 
     private void Awake()
     {
@@ -17,17 +22,17 @@ public class PlayerAnimationHandler : MonoBehaviour
 
     public void SetPlayerDirection(Vector2 normalizedDir)
     {
-        _animator.SetFloat("velocity_x", normalizedDir.x, animationConfig.moveDampTime, Time.deltaTime);
-        _animator.SetFloat("velocity_z", normalizedDir.y, animationConfig.moveDampTime, Time.deltaTime);
+        _animator.SetFloat(VelocityX, normalizedDir.x, animationConfig.moveDampTime, Time.deltaTime);
+        _animator.SetFloat(VelocityZ, normalizedDir.y, animationConfig.moveDampTime, Time.deltaTime);
     }
 
     public void StartAttackAnimation()
     {
-        _animator.SetTrigger("Attack");
+        _animator.SetTrigger(Attack);
     }
 
     public void SetAttackProgress(float progress)
     {
-        _animator.SetFloat("attackProgress", progress);
+        _animator.SetFloat(AttackProgress, progress);
     }
 }
